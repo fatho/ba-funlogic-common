fbf5f1d7f8107e669abca015a829c797548068fb
Fabian Thorand <f.thorand@gmail.com>
2014-11-16 15:55:47 +0100

    Fix for SaLT pretty printer

--- language-salt/salt-pretty/Main.hs
+++ language-salt/salt-pretty/Main.hs
@@ -6,7 +6,8 @@ import           Text.PrettyPrint.ANSI.Leijen hiding ((<$>))
 import           Text.Trifecta.Result
 
 import           FunLogic.Core.Pretty
-import           Language.SaLT.ModBuilder
+import           FunLogic.Core.ModBuilder
+import           Language.SaLT.AST
 import           Language.SaLT.Parser
 import           Language.SaLT.Pretty
 
@@ -20,8 +21,8 @@ main = do
   where
   prettyPrintFile wid file = parseSaltFileEx file >>= \case
     Failure msg -> putDoc $ msg <> line
-    Success decls -> case buildModule decls of
-      Left msg  -> putDoc . red . text $ msg
+    Success decls -> case buildModule "Main" [adt | DData adt <- decls] [bnd | DTop bnd <- decls] of
+      Left msg  -> putDoc $ red msg
       Right m -> do
         putDoc (green . text $ "Pretty printing `" ++ file ++ "`:" ++ "\n------\n")
         displayPretty wid (prettyModule m)
