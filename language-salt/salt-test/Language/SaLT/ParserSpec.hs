002ccd2a86b2ba1f2dd274c1f6f02736db0abd50
Fabian Thorand <f.thorand@gmail.com>
2015-02-10 15:24:46 +0100

    Adapted SaLT test-suite to work with prelude.

--- language-salt/salt-test/Language/SaLT/ParserSpec.hs
+++ language-salt/salt-test/Language/SaLT/ParserSpec.hs
@@ -1,8 +1,8 @@
+{-# LANGUAGE LambdaCase #-}
 module Language.SaLT.ParserSpec where
 
-import Test.Hspec
+import           Test.Hspec
 
 spec :: Spec
-spec =
-    it "can parse test files"
-      pending
+spec = it "test specific edge-cases of parser" $
+  pendingWith "whole-file parsing is part of the type-checker test-suite"
--- language-salt/salt-test/Language/SaLT/TypeCheckerSpec.hs
+++ language-salt/salt-test/Language/SaLT/TypeCheckerSpec.hs
@@ -2,45 +2,62 @@
 module Language.SaLT.TypeCheckerSpec where
 
 import           Control.Monad
+import           Control.Monad.IO.Class
 import           Data.Default.Class
-
-import           FunLogic.Core.ModBuilder
-import           FunLogic.Core.Pretty
-import           FunLogic.Core.TypeChecker
-
-import           Language.SaLT.AST
-import           Language.SaLT.Parser
-import           Language.SaLT.TypeChecker
+import qualified System.Directory             as Dir
+import qualified System.FilePath              as File
+import           Test.Hspec
 import           Text.PrettyPrint.ANSI.Leijen (pretty)
+import           Text.Printf
 import           Text.Trifecta.Result
 
-import           Test.Hspec
-import           Text.Printf
+import           FunLogic.Core.Pretty
+import           FunLogic.Core.TypeChecker
+import           Language.SaLT.AST
+import           Language.SaLT.ModBuilder
+import           Language.SaLT.Parser
+import           Language.SaLT.Prelude
+import           Language.SaLT.TypeChecker
 
+-- | Returns true when the file name's extension matches the first argument.
+fileExtensionIs :: String -> FilePath -> Bool
+fileExtensionIs ext path = File.takeExtension path == ext
+
+-- | Returns all files in a given directory matching the predicate.
+getDirectoryFiles :: MonadIO m => (FilePath -> m Bool) -> FilePath -> m [FilePath]
+getDirectoryFiles p = liftIO . Dir.getDirectoryContents >=> filterM (liftIO . Dir.doesFileExist) >=> filterM p
+
+-- | Extends scope with built-ins and prelude module.
+prepareTC :: TC SaltErrCtx ()
+prepareTC = includeBuiltIns >> unsafeIncludeModule preludeModule
+
+-- | States the expectation that the given expression has a specific type when type-checked with the default-prelude.
 shouldHaveType :: Exp -> Type -> Expectation
-shouldHaveType e ty = case evalTC (includeBuiltIns >> checkExp e) def def of
+shouldHaveType e ty = case evalTC (prepareTC >> checkExp e) def def of
   Left msg -> expectationFailure $ show $ pretty msg
   Right ty' -> unless (ty' == ty) $
     expectationFailure $ printf "Expected type `%s`, but got `%s`" (show $ prettyType ty) (show $ prettyType ty')
 
+-- | States the expectation, that the given expression has no valid type.
 shouldNotPassTC :: Exp -> Expectation
-shouldNotPassTC e = shouldFail (evalTC (includeBuiltIns >> checkExp e) def def)
+shouldNotPassTC e = shouldFail (evalTC (prepareTC >> checkExp e) def def)
 
+-- | States the expectation, that a result should be an error message.
 shouldFail :: Show a => Either e a -> Expectation
 shouldFail (Left _) = return ()
 shouldFail (Right a) = expectationFailure $ printf "Should have failed, but returned " (show a)
 
+-- | Type-checker spec.
 spec :: Spec
 spec = do
   describe "accepts valid test files" $ do
-    -- TODO: automatically determine files in "salt/valid" folder
-    let files = ["salt/valid/Test.salt", "salt/valid/ADT.salt"]
+    files <- runIO $ getDirectoryFiles (return . fileExtensionIs ".salt") "salt/valid"
     forM_ files $ \file -> it file $
       parseSaltFileEx file >>= \case
         Failure doc -> expectationFailure $ printf "could not parse file '%s': %s" file (show doc)
-        Success decls -> case buildModule "Main" [adt | DData adt <- decls] [bnd | DTop bnd <- decls] of
+        Success decls -> case buildModuleFromDecls "Main" decls of
           Left doc -> expectationFailure $ printf "could not build module from file '%s': %s" file (show doc)
-          Right modul -> case evalTC (includeBuiltIns >> checkModule modul) def def of
+          Right modul -> case evalTC (prepareTC >> checkModule modul) def def of
             Left msg -> expectationFailure $ printf "type checker failed on '%s': %s" file (show $ pretty msg)
             Right () -> return ()
 
