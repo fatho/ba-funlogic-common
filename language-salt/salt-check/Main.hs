68a6db72666bac5e2abc59d4019354fc5644db5e
Fabian Zaiser <fabian.zaiser@googlemail.com>
2015-03-02 14:02:35 +0100

    Include translated CuMin prelude as an alternative SaLT prelude.

--- language-salt/salt-check/Main.hs
+++ language-salt/salt-check/Main.hs
@@ -4,6 +4,7 @@ module Main where
 import           Control.Monad.Writer
 import           Data.Default.Class
 import           FunLogic.Core.TypeChecker
+import           Language.SaLT.AST
 import           Language.SaLT.ModBuilder
 import           Language.SaLT.Prelude
 import           Language.SaLT.TypeChecker
@@ -14,14 +15,20 @@ import           System.Environment           (getArgs)
 
 main :: IO ()
 main = do
-  (_, doc) <- runWriterT $ lift getArgs >>= mapM_ checkFile
+  putStrLn "Usage: salt-check [--cumin-prelude] FILES ..."
+  args <- getArgs
+  let (files, prelude) =
+        case args of
+          "--cumin-prelude":files -> (files, cuminPreludeModule)
+          _ -> (args, preludeModule)
+  (_, doc) <- runWriterT $ mapM_ (checkFile prelude) files
   PP.putDoc doc
 
-checkFile :: FilePath -> WriterT PP.Doc IO ()
-checkFile saltFile = do
+checkFile :: Module -> FilePath -> WriterT PP.Doc IO ()
+checkFile prelude saltFile = do
   tell $ PP.dullyellow (PP.text "Checking " <> PP.text saltFile) <> PP.text "..." <> PP.line
   buildModuleFromFile saltFile >>= \case
     Left msg    -> tell msg
-    Right modul -> case evalTC (unsafeIncludeModule preludeModule >> checkModule modul) def def of
+    Right modul -> case evalTC (unsafeIncludeModule prelude >> checkModule modul) def def of
       Left msg -> tell $ PP.pretty msg <> PP.line
       Right () -> tell $ PP.dullgreen $ PP.text "Success!" <> PP.line
--- language-salt/src/Language/SaLT/Prelude.hs
+++ language-salt/src/Language/SaLT/Prelude.hs
@@ -5,6 +5,10 @@ module Language.SaLT.Prelude where
 import           Language.SaLT.AST
 import           Language.SaLT.TH
 
--- | A CuMin module containing prelude definitions.
+-- | A SaLT module containing prelude definitions.
 preludeModule :: Module
 preludeModule = $(moduleFromFile "salt/Prelude.salt")
+
+-- | A SaLT module containing the translated CuMin prelude definitions.
+cuminPreludeModule :: Module
+cuminPreludeModule = $(moduleFromFile "salt/CuMinPrelude.salt")
